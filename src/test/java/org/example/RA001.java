8a60f00764af71603cde6c5ce6df266c1a45e809
crisking006 <criskingofhappy@gmail.com>
2024-10-04 19:10:05 +0530

    ATBX7RestAssured

--- src/test/java/org/example/RA001.java
+++ src/test/java/org/example/RA001.java
@@ -4,6 +4,11 @@ import io.restassured.RestAssured;
 
 public class RA001 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World ");
+        RestAssured
+                .given().baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1").log().all()
+                        .when().get()
+                        .then().statusCode(200);
     }
 }
