82f0397567fe511b05ce40cce4991b9f774b63ac
crisking006 <criskingofhappy@gmail.com>
2024-10-06 21:59:00 +0530

    ATBX7RestAssured

--- src/test/java/org/example/RA001.java
+++ src/test/java/org/example/RA001.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
 
 public class RA001 {
     public static void main(String[] args) {
@@ -10,5 +12,7 @@ public class RA001 {
                 .basePath("/booking/1").log().all()
                         .when().get()
                         .then().statusCode(200);
+        RequestSpecification r= RestAssured.given();
+        r.basePath("");
     }
 }
